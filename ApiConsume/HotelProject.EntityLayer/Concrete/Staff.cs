b8c1266ea3e5fddaebfbf41f9787d7e8e4e7a61d
Sadikcan Tuluk <sadikcantuluk@gmail.com>
2023-08-13 22:44:30 +0300

    Layers created

---
+++ ApiConsume/HotelProject.EntityLayer/Concrete/Staff.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelProject.EntityLayer.Concrete
+{
+    public class Staff
+    {
+        public int StaffId { get; set; }
+        public string Name { get; set; }
+        public string Title { get; set; }
+        public string Image { get; set; }
+        public string ScialMedia1 { get; set; }
+        public string ScialMedia2 { get; set; }
+        public string ScialMedia3 { get; set; }
+    }
+}
