8d92836eca538e50713c62e1560622bf120fa6cf
Sadikcan Tuluk <sadikcantuluk@gmail.com>
2023-08-28 22:39:17 +0300

    View Components are coded

---
+++ Frontend/HotelProject.WebUI/ViewvComponents/Default/_HeaderPartial.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Security.Cryptography.X509Certificates;
+
+namespace HotelProject.WebUI.ViewvComponents.Default
+{
+    public class _HeaderPartial:ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
