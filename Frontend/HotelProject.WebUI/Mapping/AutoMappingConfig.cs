497cb36f1488e2f08f5ce03a7752131cb709670c
Sadikcan Tuluk <sadikcantuluk@gmail.com>
2023-08-30 20:21:11 +0300

    Room and Trailer areas have been arranged.

--- Frontend/HotelProject.WebUI/Mapping/AutoMappingConfig.cs
+++ Frontend/HotelProject.WebUI/Mapping/AutoMappingConfig.cs
@@ -2,6 +2,7 @@
 using HotelProject.EntityLayer.Concrete;
 using HotelProject.WebUI.Dtos.LoginDto;
 using HotelProject.WebUI.Dtos.RegisterDto;
+using HotelProject.WebUI.Dtos.RoomDto;
 using HotelProject.WebUI.Dtos.ServiceDto;
 
 namespace HotelProject.WebUI.Mapping
@@ -13,9 +14,14 @@ namespace HotelProject.WebUI.Mapping
             CreateMap<CreateServiceDto, Service>().ReverseMap();
             CreateMap<ResultServiceDto, Service>().ReverseMap();
             CreateMap<UpdateServiceDto, Service>().ReverseMap();
+            CreateMap<HomeResultServiceDto, Service>().ReverseMap();
 
             CreateMap<CreateNewUserDto, AppUser>().ReverseMap();
             CreateMap<LoginUserDto, AppUser>().ReverseMap();
+
+
+            CreateMap<ResultRoomDto, Room>().ReverseMap();
+
         }
     }
 }
