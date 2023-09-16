49eb288a29144a9ed068e139e8b3c6290da8574f
Sadikcan Tuluk <sadikcantuluk@gmail.com>
2023-09-16 17:16:53 +0300

    Footer edited.

--- Frontend/HotelProject.WebUI/Controllers/DefaultController.cs
+++ Frontend/HotelProject.WebUI/Controllers/DefaultController.cs
@@ -1,11 +1,43 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using HotelProject.WebUI.Dtos.AboutDto;
+using HotelProject.WebUI.Dtos.ServiceDto;
+using HotelProject.WebUI.Dtos.SubscribeDto;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.CodeAnalysis.CSharp;
+using Newtonsoft.Json;
+using System.Text;
 
 namespace HotelProject.WebUI.Controllers
 {
     public class DefaultController : Controller
     {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public DefaultController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
         public IActionResult Index()
         {
+            return View();
+        }
+
+        [HttpGet]
+        public PartialViewResult _NewsletterPartial()
+        {
+            return PartialView();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> _NewsletterPost(CreateSubscribeDto createSubscribeDto)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var jsonData = JsonConvert.SerializeObject(createSubscribeDto);
+            StringContent content = new StringContent(jsonData, Encoding.UTF8, "application/json");
+            var responseMessage = await client.PostAsync("http://localhost:5150/api/Subscribe", content);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index", "Default");
+            }
             return View();
         }
     }
