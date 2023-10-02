e7a57c9b007ba9c44762cd2e35aed61fb0a1a6fc
Sadikcan Tuluk <sadikcantuluk@gmail.com>
2023-10-02 16:24:34 +0300

    Listing of hotels according to Id information with Booking API.

---
+++ RapidApi/RapidApiConsume/Controllers/BookingApiController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using RapidApiConsume.Models;
+using System.Net.Http.Headers;
+
+
+namespace RapidApiConsume.Controllers
+{
+    public class BookingApiController : Controller
+    {
+        public async Task<IActionResult> Index()
+        {
+            var client = new HttpClient();
+            var request = new HttpRequestMessage
+            {
+                Method = HttpMethod.Get,
+                RequestUri = new Uri("https://booking-com.p.rapidapi.com/v2/hotels/search?order_by=popularity&adults_number=2&checkin_date=2023-10-23&filter_by_currency=AED&dest_id=-1456928&locale=en-gb&checkout_date=2023-10-28&units=metric&room_number=1&dest_type=city&include_adjacency=true&page_number=0&children_ages=5%2C0&categories_filter_ids=class%3A%3A2%2Cclass%3A%3A4%2Cfree_cancellation%3A%3A1"),
+                Headers =
+    {
+        { "X-RapidAPI-Key", "9284c70ef1msh3396da3a7d254c0p104f22jsna3bd8d1c3078" },
+        { "X-RapidAPI-Host", "booking-com.p.rapidapi.com" },
+    },
+            };
+            using (var response = await client.SendAsync(request))
+            {
+                response.EnsureSuccessStatusCode();
+                var body = await response.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<BookingApiViewModel>(body);
+                return View(values.results.ToList());
+            }
+        }
+    }
+}
---
+++ RapidApi/RapidApiConsume/Controllers/BookingApiSearchLocationController.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using RapidApiConsume.Models;
+using System.Net.Http.Headers;
+
+namespace RapidApiConsume.Controllers
+{
+    public class BookingApiSearchLocationController : Controller
+    {
+        public async Task<IActionResult> Index(string cityname)
+        {
+            if (!string.IsNullOrWhiteSpace(cityname))
+            {
+                List<BookingApiSearchLocationViewModel> bookingApiSearchLocationViewModel = new List<BookingApiSearchLocationViewModel>();
+                var client = new HttpClient();
+                var request = new HttpRequestMessage
+                {
+                    Method = HttpMethod.Get,
+                    RequestUri = new Uri($"https://booking-com.p.rapidapi.com/v1/hotels/locations?name={cityname}&locale=en-gb"),
+                    Headers =
+    {
+        { "X-RapidAPI-Key", "9284c70ef1msh3396da3a7d254c0p104f22jsna3bd8d1c3078" },
+        { "X-RapidAPI-Host", "booking-com.p.rapidapi.com" },
+    },
+                };
+                using (var response = await client.SendAsync(request))
+                {
+                    response.EnsureSuccessStatusCode();
+                    var body = await response.Content.ReadAsStringAsync();
+                    bookingApiSearchLocationViewModel = JsonConvert.DeserializeObject<List<BookingApiSearchLocationViewModel>>(body);
+                    return View(bookingApiSearchLocationViewModel.Take(1).ToList());
+
+                }
+            }
+            else
+            {
+                List<BookingApiSearchLocationViewModel> bookingApiSearchLocationViewModel = new List<BookingApiSearchLocationViewModel>();
+                var client = new HttpClient();
+                var request = new HttpRequestMessage
+                {
+                    Method = HttpMethod.Get,
+                    RequestUri = new Uri("https://booking-com.p.rapidapi.com/v1/hotels/locations?name=paris&locale=en-gb"),
+                    Headers =
+    {
+        { "X-RapidAPI-Key", "9284c70ef1msh3396da3a7d254c0p104f22jsna3bd8d1c3078" },
+        { "X-RapidAPI-Host", "booking-com.p.rapidapi.com" },
+    },
+                };
+                using (var response = await client.SendAsync(request))
+                {
+                    response.EnsureSuccessStatusCode();
+                    var body = await response.Content.ReadAsStringAsync();
+                    bookingApiSearchLocationViewModel = JsonConvert.DeserializeObject<List<BookingApiSearchLocationViewModel>>(body);
+                    return View(bookingApiSearchLocationViewModel.Take(1).ToList());
+
+                }
+            }
+
+        }
+    }
+}
