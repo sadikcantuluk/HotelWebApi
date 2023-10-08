9f88c3df3809e3ab38c80d9bc6b6010151003657
Sadikcan Tuluk <sadikcantuluk@gmail.com>
2023-10-08 14:10:27 +0300

    Admin side e-mail sending process has been completed.

---
+++ Frontend/HotelProject.WebUI/Controllers/AdminMailController.cs
@@ -0,0 +1,43 @@
+﻿using HotelProject.WebUI.Models.Mail;
+using MailKit.Net.Smtp;
+using Microsoft.AspNetCore.Mvc;
+using MimeKit;
+using Org.BouncyCastle.Crypto.Macs;
+using System.Net.Mail;
+
+namespace HotelProject.WebUI.Controllers
+{
+    public class AdminMailController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(AdminMailViewModel adminMailViewModel)
+        {
+            MimeMessage mimeMessage = new MimeMessage();
+
+            MailboxAddress mailboxAddressFrom = new MailboxAddress("Hotelier Admin", "bedava452@gmail.com");
+            mimeMessage.From.Add(mailboxAddressFrom);
+
+            MailboxAddress mailboxAddressTo = new MailboxAddress(adminMailViewModel.RecieverName, adminMailViewModel.RecieverMail);
+            mimeMessage.To.Add(mailboxAddressTo);
+
+            var bodyBuilder = new BodyBuilder();
+            bodyBuilder.TextBody = adminMailViewModel.Message;
+            mimeMessage.Body = bodyBuilder.ToMessageBody();
+
+            mimeMessage.Subject = adminMailViewModel.Subject;
+
+            MailKit.Net.Smtp.SmtpClient smtpClient = new MailKit.Net.Smtp.SmtpClient();
+            smtpClient.Connect("smtp.gmail.com", 587, false);
+            smtpClient.Authenticate("bedava452@gmail.com", "wkvjtmkqxgxuamjr");
+            smtpClient.Send(mimeMessage);
+            smtpClient.Disconnect(true);
+            return View();
+        }
+    }
+}
---
+++ Frontend/HotelProject.WebUI/Models/Mail/AdminMailViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace HotelProject.WebUI.Models.Mail
+{
+    public class AdminMailViewModel
+    {
+        public string RecieverName { get; set; }
+        public string RecieverMail { get; set; }
+        public string SenderName { get; set; }
+        public string SenderMail { get; set; }
+        public string Subject { get; set; }
+        public string Message { get; set; }
+    }
+}
