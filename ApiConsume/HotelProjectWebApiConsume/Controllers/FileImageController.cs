df0725da48e31937f08719c704dace719b823008
Sadikcan Tuluk <sadikcantuluk@gmail.com>
2023-10-11 20:14:21 +0300

    API file operations are completed.

---
+++ ApiConsume/HotelProjectWebApiConsume/Controllers/FileImageController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HotelProjectWebApiConsume.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FileImageController : ControllerBase
+    {
+        [HttpPost]
+        public async Task<IActionResult> UploudImage([FromForm]IFormFile file)
+        {
+            var fileName = Guid.NewGuid() + Path.GetExtension(file.FileName);
+            var path = Path.Combine(Directory.GetCurrentDirectory(), "files/" + fileName);
+            var stream = new FileStream(path, FileMode.Create);
+            await file.CopyToAsync(stream);
+            return Created("", file);
+        }
+    }
+}
---
+++ Frontend/HotelProject.WebUI/Controllers/AdminImageFileController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Net.Http.Headers;
+
+namespace HotelProject.WebUI.Controllers
+{
+    public class AdminImageFileController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Index(IFormFile file)
+        {
+            var stream = new MemoryStream();
+            await file.CopyToAsync(stream);
+            var bytes = stream.ToArray();
+
+            ByteArrayContent byteArrayContent = new ByteArrayContent(bytes);
+            byteArrayContent.Headers.ContentType = new MediaTypeHeaderValue(file.ContentType);
+            MultipartFormDataContent multipartFormDataContent = new MultipartFormDataContent();
+            multipartFormDataContent.Add(byteArrayContent, "file", file.FileName);
+
+            HttpClient client = new HttpClient();
+            var responseMessage = await client.PostAsync("http://localhost:5150/api/FileImage", multipartFormDataContent);
+
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return View();
+            }
+
+
+            return View();
+        }
+    }
+}
