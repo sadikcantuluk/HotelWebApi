9de8af3f9ff70ac17cd0bb47e3667c955fb5d4f8
Sadikcan Tuluk <sadikcantuluk@gmail.com>
2023-10-12 20:43:47 +0300

    404 Error Page designed.

--- Frontend/HotelProject.WebUI/Program.cs
+++ Frontend/HotelProject.WebUI/Program.cs
@@ -42,6 +42,9 @@ if (!app.Environment.IsDevelopment())
     app.UseExceptionHandler("/Home/Error");
 }
 
+app.UseStatusCodePagesWithReExecute("/ErrorPage/Error404", "?code={0}");
+app.UseHttpsRedirection();
+
 app.UseStaticFiles();
 
 app.UseAuthentication();
